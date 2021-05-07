5093b41bfb77451a87c7ea369e73055729758fc1
Sarah <saraheileenbeck@gmail.com>
2021-05-06 21:29:54 -0700

    fixed mobile modal. added comments to js file

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+//Recognizes when user clicks "Contact" and opens window
 document.getElementById('nav-contact').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'flex';
+    
 });
 
+//Closes window when "x" is clicked
 document.querySelector('.close').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'none';
 });
 
+//Closes window when "submit" is clicked
 document.getElementById('modal-submit').addEventListener('click', function() {
     document.querySelector('.bg-modal').style.display = 'none';
 });
+
